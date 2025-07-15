a31aaf5ae955e567f2e41d82e8097a7e3b236d98
Stéphane Gerard <stgerard@vub.ac.be>
2025-07-15 13:24:57 +0200

    condor_version facts plugin is deployed everywhere but on machines

--- lib/facter/condor_version.rb
+++ lib/facter/condor_version.rb
@@ -4,6 +4,7 @@
 #
 Facter.add(:condor_version) do
   setcode do
+    next nil unless Facter::Util::Resolution.which('condor_version')
     Facter::Util::Resolution.exec('condor_version 2>&1').split('\n')[0].split(' ')[1]
   end
 end
